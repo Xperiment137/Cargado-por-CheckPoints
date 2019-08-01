2ce788209ca1571c389b233a7b3697c8593675e4
muerteloca12 <43545742+muerteloca12@users.noreply.github.com>
2019-08-01 14:18:04 +0200

    Add files via upload

---
+++ Sistema de Checkpoint/Gusrdadoch.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine.SceneManagement;
+public class Gusrdadoch : MonoBehaviour
+{
+    public GameObject aux2;
+    private Vidamanager vid;
+    private float x;
+    private float y;
+    private float z;
+   public string escena;
+    void Awake()
+    {
+       
+       ;
+        aux2 = GameObject.Find("Jugador");
+        vid = aux2.GetComponent<Vidamanager>();
+        
+    }
+    void Update()
+    {
+      
+
+    }
+
+        public static void SaveInventario()
+    {
+
+        BinaryFormatter bf = new BinaryFormatter();
+        Stream filein = new FileStream(Application.persistentDataPath + "Guardado", FileMode.Create);
+        bf.Serialize(filein, ClaseInventario.Invs);
+        filein.Close();
+
+
+    }
+    public static void LoadInventario()
+    {
+        if (File.Exists(Application.persistentDataPath + "Guardado"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "Guardado" ,FileMode.Open);
+            ClaseInventario.Invs = (List<ClaseInventario>)bf.Deserialize(file);
+            file.Close();
+        }
+    }
+    public void GuardarEscena()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        PlayerPrefs.SetString("Escena",scene.name );
+    }
+   public  IEnumerator LoadYourAsyncScene()
+    {
+        escena = PlayerPrefs.GetString("Escena");
+        if(escena==SceneManager.GetActiveScene().ToString())
+        {
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(escena, LoadSceneMode.Single);
+            while (!asyncLoad.isDone)
+            {
+                yield return null;
+            }
+            CargarJugador();
+            SceneManager.MoveGameObjectToScene(aux2, SceneManager.GetSceneByName(escena));
+           
+        }
+        else
+        {
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(escena, LoadSceneMode.Additive);
+            while (!asyncLoad.isDone)
+            {
+                yield return null;
+            }
+            CargarJugador();
+            SceneManager.MoveGameObjectToScene(aux2, SceneManager.GetSceneByName(escena));
+            SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+        }
+      
+    }
+        public bool CargarJugador()
+    {
+        x = PlayerPrefs.GetFloat("x");
+        y = PlayerPrefs.GetFloat("y");
+        z = PlayerPrefs.GetFloat("z");
+        Vector3 posVec = new Vector3(x, y, z);
+        aux2.transform.position = posVec;
+        vid.Vida= PlayerPrefs.GetInt("Vida");
+        return true;
+    }
+        public void GuardarJugador()
+    {
+        PlayerPrefs.SetInt("Vida", vid.Vida);
+        PlayerPrefs.SetFloat("x",aux2.transform.position.x);
+        PlayerPrefs.SetFloat("y", aux2.transform.position.y);
+        PlayerPrefs.SetFloat("z", aux2.transform.position.z);
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+
+            GuardarEscena();
+            GuardarJugador();
+          
+
+        }
+
+
+
+    }
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Sistema de Checkpoint/Menu.cs
@@ -0,0 +1,194 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Threading;
+public class Menu : MonoBehaviour
+{
+    public Button boton, boton1, boton2;
+    public GameObject aux,aux2,aux3,aux4,aux5;
+    private bool activo, activo2, activo4;
+    private string mensaje;
+    private Gusrdadoch car;
+    void Awake()
+    {
+        Cursor.visible = true;
+        Time.timeScale = 1;
+        aux = GameObject.Find("Menudeescena");
+        aux2 = GameObject.Find("OpcionesPanel");
+        aux3 = GameObject.Find("CargadorPanel");
+        aux4 =GameObject.Find("CheckPoint");
+        aux5 = GameObject.Find("PantallaCarga");
+       
+        car = aux4.GetComponent<Gusrdadoch>();
+    }
+    void Start()
+    {
+        aux.SetActive(false);
+        aux2.SetActive(false);
+        aux3.SetActive(false);
+        aux5.SetActive(false);
+        activo4 = false;
+    }
+
+
+    private void Update()
+    {
+       
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (aux.activeSelf)
+            {
+                ContinueGame();
+}
+            else
+            {
+
+                PauseGame();
+                boton.onClick.AddListener(() => Reiniciar(false));
+                boton1.onClick.AddListener(() => Cargar());
+             
+               
+
+            }
+
+            if (aux2.activeSelf)
+            {
+
+                aux2.SetActive(false);
+
+
+            }
+            if (aux3.activeSelf)
+            {
+
+                aux3.SetActive(false);
+
+
+            }
+
+        }
+            }
+
+
+    private void Reiniciar(bool seguro)
+    {
+        if (seguro)
+        {
+             string escena = PlayerPrefs.GetString("Escena");
+            SceneManager.LoadScene(escena, LoadSceneMode.Single);
+        }
+        else
+        {
+            activo = true;
+        }
+
+    }
+    private void Cargar()
+    {
+        aux3.SetActive(true);
+        activo2 = true;
+        if (aux4)
+        {
+            aux3.SetActive(false);
+            aux5.SetActive(true);
+            mensaje = "Cargando";
+            activo4 = true;
+
+        }
+            if(car.CargarJugador())
+            {
+               
+                Thread.Sleep(25);
+                mensaje = "Listo";
+                activo4 =false;
+                aux5.SetActive(false);
+                aux3.SetActive(true);
+
+            }
+        
+           
+           
+        }
+        
+    
+
+
+    void OnGUI()
+    {
+
+        if (activo)
+        {
+
+            GUI.skin.label.alignment = TextAnchor.UpperCenter;
+            GUI.Label(new Rect(Screen.width / 2 - 300, 50, 500, 50), "¿Estas seguro...?");
+            using (var areaScope = new GUILayout.AreaScope(new Rect(Screen.width / 2 - 300, 100, 500, 50)))
+            {
+
+
+                if (GUILayout.Button("Si"))
+                {
+                    Reiniciar(true);
+
+                }
+                if (GUILayout.Button("No"))
+                {
+                    aux3.SetActive(false);
+                    activo2 = false;
+                }
+            }
+        }
+
+
+        if (activo2)
+
+        {
+
+
+            GUI.skin.label.alignment = TextAnchor.UpperCenter;
+            GUI.Label(new Rect(Screen.width / 2 - 400, 50, 500, 50), "¿Seguro?");
+
+            using (var areaScope = new GUILayout.AreaScope(new Rect(Screen.width / 2 - 300, 100, 500, 50)))
+            {
+
+                if (GUILayout.Button("Si"))
+                {
+                    activo2 = false;
+                    
+
+                }
+                if (GUILayout.Button("No"))
+                {
+                    activo2 = false;
+
+                }
+            }
+
+                if (activo4)
+                {
+                    GUI.Label(new Rect(Screen.width / 2 - 300, 50, 500, 50), mensaje);
+                  
+
+                }
+            }
+    }
+    private void PauseGame()
+    {
+
+
+        aux.SetActive(true);
+
+    }
+
+    private void ContinueGame()
+    {
+        Cursor.visible = false;
+        aux.SetActive(false);
+        Time.timeScale = 1;
+
+
+
+    }
+}
